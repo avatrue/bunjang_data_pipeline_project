69bc50abb52c6c9f37d88bcfd3c232ad68fced5a
6-6ho <6_6ho@kakao.com>
2024-05-22 18:59:52 +0900

    [update] Updated YfinanceDataFetcher module to fetch NASDAQ, KOSPI, and KOSDAQ"

---
+++ airflow/modules/yfinance_data_fetcher.py
@@ -0,0 +1,47 @@
+import yfinance as yf
+import logging
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+class YfinanceDataFetcher:
+    def __init__(self) -> None:
+        self.nasdaq_ticker = "^IXIC"
+        self.kospi_ticker = "^KS11"
+        self.kosdaq_ticker = "^KQ11"
+
+    def get_data(self, ticker: str, date: datetime) -> pd.DataFrame:
+        logging.info(f"Requesting data for {ticker} on {date.strftime('%Y-%m-%d')}")
+        market_data = yf.download(
+            ticker,
+            start=date.strftime("%Y-%m-%d"),
+            end=(date + timedelta(days=1)).strftime("%Y-%m-%d"),
+        )
+
+        market_data = market_data[market_data.index.date == date.date()]
+
+        if market_data.empty:
+            return self.get_previous_data(ticker, date)
+        return market_data
+
+    def get_previous_data(self, ticker: str, date: datetime) -> pd.DataFrame:
+        while True:
+            date -= timedelta(days=1)
+            previous_date = date.strftime("%Y-%m-%d")
+            logging.info(f"Requesting previous data for {ticker} on {previous_date}")
+            market_data = yf.download(
+                ticker,
+                start=previous_date,
+                end=(date + timedelta(days=1)).strftime("%Y-%m-%d"),
+            )
+            if not market_data.empty:
+                return market_data
+
+    def get_nasdaq_data(self, date: datetime) -> pd.DataFrame:
+        return self.get_data(self.nasdaq_ticker, date)
+
+    def get_kospi_data(self, date: datetime) -> pd.DataFrame:
+        return self.get_data(self.kospi_ticker, date)
+
+    def get_kosdaq_data(self, date: datetime) -> pd.DataFrame:
+        return self.get_data(self.kosdaq_ticker, date)
